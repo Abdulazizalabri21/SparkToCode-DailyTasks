7b07b3cc25f9623c37c79f8579f4d7ddb712ce1f
Abdulaziz Rashid <Abdulazizalabri08@gimail.com>
2026-07-02 13:22:55 +0400

    Task 2 - Rectangle Calculator

--- C# Fundamentals - Part 1/Program.cs
+++ C# Fundamentals - Part 1/Program.cs
@@ -13,6 +13,24 @@
             Console.WriteLine("Name:" + personalname + " Age:" + age + " height:" + height + " student:" + studentornot);
             //task-1---------------------------------------------------------------------------------------------
 
+
+
+
+            //task-2---------------------------------------------------------------------------------------------
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Plese enter the length ");
+            Double length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Plese enter the Width ");
+            Double Width = double.Parse(Console.ReadLine());
+
+            double Area = length * Width;
+            double Perimeter = 2 * (length + Width);
+
+            Console.WriteLine("the area = " + Area);
+            Console.WriteLine("the Perimeter = " + Perimeter);
+            //task-2---------------------------------------------------------------------------------------------
         }
     }
 }
