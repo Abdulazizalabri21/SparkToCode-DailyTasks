baff2af2a66de017861e6638170aa8058de354b9
Abdulaziz Rashid <Abdulazizalabri08@gimail.com>
2026-07-03 13:33:07 +0400

    Task 2 - Sum of Numbers 1 to N

--- C# Fundamentals - Part 2/Program.cs
+++ C# Fundamentals - Part 2/Program.cs
@@ -4,18 +4,37 @@
     {
         static void Main(string[] args)
         {
-            //task-1---------EASY ----------------------------------------------------------------------------
+            ////task-1---------EASY ----------------------------------------------------------------------------
 
-            Console.WriteLine("Enter the starting number. ");
-            int STnumber =int.Parse(Console.ReadLine());
-            Console.WriteLine(" ");
+            //Console.WriteLine("Enter the starting number. ");
+            //int STnumber =int.Parse(Console.ReadLine());
+            //Console.WriteLine(" ");
 
-            for (int Snumber = STnumber ; STnumber >= 1; STnumber--)
-            { 
-            Console.WriteLine(STnumber + "  " );
+            //for (int Snumber = STnumber ; STnumber >= 1; STnumber--)
+            //{ 
+            //Console.WriteLine(STnumber + "  " );
+            //}
+            //Console.WriteLine("Liftoff");
+            ////task-1--------------EASY---------------------------------------------------------------------------
+
+
+
+            //task-2--------------EASY-------------------------------------------------------
+            
+            Console.WriteLine("Enter the number (positive number only). ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= number; i++) 
+            {
+                sum += i;
             }
-            Console.WriteLine("Liftoff");
-            //task-1--------------EASY---------------------------------------------------------------------------
+            Console.WriteLine("The sum of the numbers from 1 to " + number + " is: " + sum);
+
+
+            //task-2--------------EASY-------------------------------------------------------
+
+
+
         }
     }
 }
