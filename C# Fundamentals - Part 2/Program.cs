cf8e47520d90ba59430dd6b6c265a5f3919f035a
Abdulaziz Rashid <Abdulazizalabri08@gimail.com>
2026-07-03 14:04:40 +0400

    Task 3 - Multiplication Table

--- C# Fundamentals - Part 2/Program.cs
+++ C# Fundamentals - Part 2/Program.cs
@@ -19,22 +19,39 @@
 
 
 
-            //task-2--------------EASY-------------------------------------------------------
-            
-            Console.WriteLine("Enter the number (positive number only). ");
+            ////task-2--------------EASY-------------------------------------------------------
+
+            //Console.WriteLine("Enter the number (positive number only). ");
+            //int number = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //for (int i = 1; i <= number; i++) 
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine("The sum of the numbers from 1 to " + number + " is: " + sum);
+
+            ////task-2--------------EASY-------------------------------------------------------
+           
+            Console.WriteLine("Enter the number ");
             int number = int.Parse(Console.ReadLine());
-            int sum = 0;
-            for (int i = 1; i <= number; i++) 
+
+            for (int i = 1; i <= 10; i++)
             {
-                sum += i;
+
+                Console.WriteLine(+ number +"*" + i +"= "+ (number * i));
+                
             }
-            Console.WriteLine("The sum of the numbers from 1 to " + number + " is: " + sum);
-
-
-            //task-2--------------EASY-------------------------------------------------------
+            //task-3--------------EASY-------------------------------------------------------
 
 
 
+
+
+
+
+
+
+            //task-3--------------EASY-------------------------------------------------------
         }
     }
 }
