a3f1b52af17251255dffb93c6ff8d2bbe7227b26
Abdulaziz Rashid <Abdulazizalabri08@gimail.com>
2026-07-03 12:54:16 +0400

    Adding C# Fundamentals - Part 2 file

---
+++ C# Fundamentals - Part 2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__Fundamentals___Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
