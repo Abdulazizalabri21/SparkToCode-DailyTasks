2840d181b2a62fa1b84bd15d502ca87d0da36b9e
Abdulaziz Rashid <Abdulazizalabri08@gimail.com>
2026-07-06 15:43:25 +0400

    Task 1 - Absolute Difference

--- C# Fundamentals - Part 3/Program.cs
+++ C# Fundamentals - Part 3/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            
+
+            //task-1---------EASY ------------------------------------------------------------------------
+            Console.WriteLine("Enter the first number: ");
+            double Num1 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the first number: ");
+            double Num2 = double.Parse(Console.ReadLine());
+
+
+            double subtraction = Math.Abs(Num1 - Num2);
+            Console.WriteLine("the result of subtaction of "+Num1 +"-" + Num2 + "= " + subtraction);
+
+            //task-1---------EASY ------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
