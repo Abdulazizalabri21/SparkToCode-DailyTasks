2b7d6cb8be7f020cb2a0a833e845a1f66b13b87c
Abdulaziz Rashid <Abdulazizalabri08@gimail.com>
2026-07-06 16:22:30 +0400

    Task 2 - Power & Root Explorer

--- C# Fundamentals - Part 3/Program.cs
+++ C# Fundamentals - Part 3/Program.cs
@@ -5,21 +5,33 @@
         static void Main(string[] args)
         {
 
-            //task-1---------EASY ------------------------------------------------------------------------
-            Console.WriteLine("Enter the first number: ");
-            double Num1 = double.Parse(Console.ReadLine());
+            ////task-1---------EASY ------------------------------------------------------------------------
+            //Console.WriteLine("Enter the first number: ");
+            //double Num1 = double.Parse(Console.ReadLine());
 
 
-            Console.WriteLine("Enter the first number: ");
-            double Num2 = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the first number: ");
+            //double Num2 = double.Parse(Console.ReadLine());
 
 
-            double subtraction = Math.Abs(Num1 - Num2);
-            Console.WriteLine("the result of subtaction of "+Num1 +"-" + Num2 + "= " + subtraction);
+            //double subtraction = Math.Abs(Num1 - Num2);
+            //Console.WriteLine("the result of subtaction of "+Num1 +"-" + Num2 + "= " + subtraction);
 
-            //task-1---------EASY ------------------------------------------------------------------------
+            ////task-1---------EASY ------------------------------------------------------------------------
+
+            //task-2---------EASY ------------------------------------------------------------------------
 
 
+            Console.WriteLine("Enter the number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            double squar = Math.Pow(number, 2);
+            double squareRoot = Math.Sqrt(number);
+
+            
+            Console.WriteLine("the square of " + number + " = " + squar);
+            Console.WriteLine("the square root of " + number + " = " + squareRoot);
+            //task-2---------EASY ------------------------------------------------------------------------
 
 
 
