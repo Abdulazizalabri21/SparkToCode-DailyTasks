b92b1dac5f022d3e5139e021fddaffdc716e3449
Abdulaziz Rashid <Abdulazizalabri08@gimail.com>
2026-07-07 14:57:32 +0400

    Task 1 - Personalized Welcome Function

--- C# Fundamentals - Part 4/Program.cs
+++ C# Fundamentals - Part 4/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace C__Fundamentals___Part_4
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace C__Fundamentals___Part_4
 {
     internal class Program
     {
+
+        //Task 1 - Personalized Welcome Function
+        public static void PrintWelcome(string name)
+        {
+            Console.WriteLine($"Welcome Mr/Ms "+ name);
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Task 1 - Personalized Welcome Function----------
+
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            PrintWelcome(name);
+            //Task 1 - Personalized Welcome Function-------
+
+
+
+
         }
     }
 }
