2145082c06c513d5329e13d2dea8495d8bf69704
Abdulaziz Rashid <Abdulazizalabri08@gimail.com>
2026-07-08 18:58:47 +0400

    Task 1 - Fixed Grades Array

--- C# Fundamentals - Part 5/Program.cs
+++ C# Fundamentals - Part 5/Program.cs
@@ -4,6 +4,26 @@
     {
         static void Main(string[] args)
         {
+            //Task 1 - Fixed Grades Array
+
+            int[] grades = new int[5];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine("Enter a grade: ");
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+
+
+             Console.WriteLine("the grades: ");
+            foreach (int i in grades)
+            {
+               
+                Console.WriteLine(i);
+            }
+
+
+            //Task 1 - Fixed Grades Array
 
 
         }
